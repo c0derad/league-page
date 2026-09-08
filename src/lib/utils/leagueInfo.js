33778a4c767a7886bbed51971127f32b7de1be94
c0derad <conradjbailey@outlook.com>
2026-09-08 09:09:01 -0500

    Change 'Hometown' to 'Location' and add 'School'
    Updated manager information fields in leagueInfo.js.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,8 @@ export const managers = [
        "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "Conrad J. Bailey III",
     //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "Hometown": "Norwell, MA", // (optional)
+       "Location": "Dallas, TX", // (optional)
+       "School": "Alabama"
        "bio": "THE CZAR",
        "photo": "/managers/CB (Custom).png", // square ratio recommended (no larger than 500x500)
     //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
