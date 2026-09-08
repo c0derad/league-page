b3b4c541bf2069a265d2934cadd0d58cd6b53d36
c0derad <conradjbailey@outlook.com>
2026-09-08 11:39:22 -0500

    Refactor ManagerRow.svelte for clarity and style
    Refactor imports, improve formatting, and update styles in ManagerRow.svelte.

--- src/lib/Managers/ManagerRow.svelte
+++ src/lib/Managers/ManagerRow.svelte
@@ -1,7 +1,11 @@
 <script>
     import { goto } from "$app/navigation";
-	import { getDatesActive, getRosterIDFromManagerID, getTeamNameFromTeamManagers } from "$lib/utils/helperFunctions/universalFunctions";
-    import {dynasty} from "$lib/utils/leagueInfo"
+    import {
+        getDatesActive,
+        getRosterIDFromManagerID,
+        getTeamNameFromTeamManagers
+    } from "$lib/utils/helperFunctions/universalFunctions";
+    import { dynasty } from "$lib/utils/leagueInfo";
 
     export let manager, leagueTeamManagers, key;
 
@@ -11,14 +15,19 @@
     let rosterID = manager.roster;
     let year = null;
 
-    if(manager.managerID) {
+    if (manager.managerID) {
         const dates = getDatesActive(leagueTeamManagers, manager.managerID);
-        if(dates.end) retired = true;
 
-        ({rosterID, year} = getRosterIDFromManagerID(leagueTeamManagers, manager.managerID) || {rosterID, year});
+        if (dates.end) retired = true;
+
+        ({ rosterID, year } =
+            getRosterIDFromManagerID(leagueTeamManagers, manager.managerID) ||
+            { rosterID, year });
     }
 
-    const commissioner = manager.managerID ? leagueTeamManagers.users[manager.managerID].is_owner : false;
+    const commissioner = manager.managerID
+        ? leagueTeamManagers.users[manager.managerID].is_owner
+        : false;
 </script>
 
 <style>
@@ -26,7 +35,7 @@
         display: flex;
         justify-content: left;
         align-items: center;
-        padding: 1em 0;
+        padding: 1em 1.25em;
         background-color: var(--fff);
         background-repeat: no-repeat;
         background-position: 15% 50%;
@@ -38,17 +47,14 @@
     }
 
     .manager:hover {
-        box-shadow: 0 0 10px 0 bar(--g999);
-        background-color: bar(--eee);
+        box-shadow: 0 0 10px 0 var(--g999);
+        background-color: var(--eee);
     }
 
-    .photo {
-        height: 40px;
-        width: 40px;
-        border-radius: 100%;
-        vertical-align: middle;
-        margin-left: 1em;
-        box-shadow: 0 0 2px 1px var(--bbb);
+    .nameHolder {
+        display: flex;
+        align-items: center;
+        gap: 0.45em;
     }
 
     .name {
@@ -56,8 +62,21 @@
         display: inline-block;
         color: var(--g555);
         line-height: 1.2em;
-        margin-left: 1em;
         font-weight: 700;
+    }
+
+    .commissionerBadge {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        height: 22px;
+        width: 22px;
+        font-size: 0.75em;
+        font-weight: 600;
+        border-radius: 50%;
+        background-color: var(--blueTwo);
+        border: 1px solid var(--blueOne);
+        color: #fff;
     }
 
     .team {
@@ -108,31 +127,9 @@
         line-height: 1.2em;
     }
 
-    .avatarHolder {
-        display: inline-flex;
-        position: relative;
-    }
-
-    .commissionerBadge {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        bottom: -10px;
-        right: -10px;
-        height: 25px;
-        width: 25px;
-        font-weight: 600;
-        border-radius: 15px;
-        background-color: var(--blueTwo);
-        border: 1px solid var(--blueOne);
-        color: #fff;
-    }
-
-	@media (max-width: 665px) {
+    @media (max-width: 665px) {
         .name {
             font-size: 0.9em;
-            margin-left: 0.5em;
         }
 
         .team {
@@ -141,23 +138,17 @@
         }
     }
 
-	@media (max-width: 595px) {
+    @media (max-width: 595px) {
         .manager {
-            padding: 0.5em 0;
+            padding: 0.5em 0.75em;
             margin: 0.3em 0;
             border-radius: 1.5em;
         }
 
-        .photo {
-            height: 30px;
-            width: 30px;
-            margin-left: 0.5em;
-        }
-
         .commissionerBadge {
-            height: 15px;
-            width: 15px;
-            font-size: 0.8em;
+            height: 18px;
+            width: 18px;
+            font-size: 0.7em;
         }
 
         .infoSlot {
@@ -184,7 +175,6 @@
     @media (max-width: 475px) {
         .name {
             font-size: 0.8em;
-            margin-left: 0.4em;
         }
 
         .team {
@@ -192,9 +182,10 @@
             margin-left: 0.4em;
         }
 
-        .photo {
-            height: 25px;
-            width: 25px;
+        .commissionerBadge {
+            height: 16px;
+            width: 16px;
+            font-size: 0.65em;
         }
 
         .infoSlot {
@@ -229,59 +220,102 @@
     }
 </style>
 
-<div class="manager" style="{retired ? "background-image: url(/retired.png); background-color: var(--ddd)": ""}" onclick={() => goto(`/manager?manager=${key}`)}>
-    <div class="avatarHolder">
-        <img class="photo" src="{manager.photo}" alt="{manager.name}" />
+<div
+    class="manager"
+    style="{retired
+        ? 'background-image: url(/retired.png); background-color: var(--ddd)'
+        : ''}"
+    onclick={() => goto(`/manager?manager=${key}`)}
+>
+    <div class="nameHolder">
+        <div class="name">{manager.name}</div>
+
         {#if commissioner}
             <div class="commissionerBadge">
                 <span>C</span>
             </div>
         {/if}
     </div>
-    <div class="name">{manager.name}</div>
-    <div class="team">{getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}</div>
+
+    <div class="team">
+        {getTeamNameFromTeamManagers(
+            leagueTeamManagers,
+            rosterID,
+            year
+        )}
+    </div>
+
     <div class="spacer" />
+
     <div class="info">
-        <!-- Favorite team (optional) -->
+        <!-- Favorite team -->
         <div class="infoSlot infoTeam">
             {#if manager.favoriteTeam}
                 <div class="infoIcon">
-                    <img class="infoImg" src="https://sleepercdn.com/images/team_logos/nfl/{manager.favoriteTeam}.png" alt="favorite team"/>
+                    <img
+                        class="infoImg"
+                        src="https://sleepercdn.com/images/team_logos/nfl/{manager.favoriteTeam}.png"
+                        alt="favorite team"
+                    />
                 </div>
             {:else}
                 <div class="infoIcon question">
-                    <img class="infoImg" src="/managers/question.jpg" alt="favorite team"/>
+                    <img
+                        class="infoImg"
+                        src="/managers/question.jpg"
+                        alt="favorite team unknown"
+                    />
                 </div>
             {/if}
         </div>
+
         <!-- Preferred contact -->
         <div class="infoSlot">
             {#if manager.preferredContact}
                 <div class="infoIcon">
-                    <img class="infoImg" src="/{manager.preferredContact}.png" alt="{manager.preferredContact}"/>
+                    <img
+                        class="infoImg"
+                        src="/{manager.preferredContact}.png"
+                        alt="{manager.preferredContact}"
+                    />
                 </div>
+
                 <div class="infoAnswer">
                     {manager.preferredContact}
                 </div>
             {:else}
                 <div class="infoIcon question">
-                    <img class="infoImg" src="/managers/question.jpg" alt="favorite team"/>
+                    <img
+                        class="infoImg"
+                        src="/managers/question.jpg"
+                        alt="preferred contact unknown"
+                    />
                 </div>
             {/if}
         </div>
-        <!-- Rebuild mode (optional and only displayed for dynasty leagues) -->
+
+        <!-- Rebuild mode -->
         {#if dynasty}
             <div class="infoSlot infoRebuild">
                 {#if manager.mode}
                     <div class="infoIcon">
-                        <img class="infoImg" src="/{manager.mode.replace(' ', '%20')}.png" alt="win now or rebuild"/>
+                        <img
+                            class="infoImg"
+                            src="/{manager.mode.replace(' ', '%20')}.png"
+                            alt="win now or rebuild"
+                        />
                     </div>
+
                     <div class="infoAnswer">
                         {manager.mode}
                     </div>
                 {:else}
                     <div class="infoIcon question">
-                        <img class="infoImg" src="/managers/question.jpg" alt="favorite team"/>
+                        <img
+                            class="infoImg"
+                            src="/managers/question.jpg"
+                            alt="team mode unknown"
+                        />
                     </div>
                 {/if}
             </div>
