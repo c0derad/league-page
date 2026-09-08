f0495af1fba79f9bbc467cf095e1e4ae2401c9e2
c0derad <conradjbailey@outlook.com>
2026-09-08 10:41:20 -0500

    Add new manager entries to leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -24,74 +24,116 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-     {
-       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "Conrad J. Bailey III",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Dallas, TX", // (optional)
-       "school": "Alabama",
-       "bio": "THE CZAR",
-       "photo": "/managers/CB (Custom).png", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
-    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
-    // {
-    //   "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
-    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
-    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    // },
-    // {
-    //   "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
-    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
-    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    // },
-  ]
+  {
+    "managerID": "873018604659118080",
+    "name": "Brad",
+    "location": "Dallas, TX",
+    "school": "Alabama",
+    "bio": "THE CZAR",
+    "photo": "/managers/conrad.png",
+    "favoriteTeam": "ne",
+  },
+  {
+    "managerID": "1132146450068205568",
+    "name": "JV",
+    "location": "Dallas, TX",
+    "school": "Penn State",
+    "bio": "Peter Pan",
+    "photo": null,
+    "favoriteTeam": "pit",
+  },
+  {
+    "managerID": "1132146764921982976",
+    "name": "Chaney",
+    "location": "Dallas, TX",
+    "school": "TCU",
+    "bio": "Chain Gang",
+    "photo": null,
+    "favoriteTeam": "dal",
+  },
+  {
+    "managerID": "1132146859176460288",
+    "name": "Trevor",
+    "location": "Dallas, TX",
+    "school": "Alabama",
+    "bio": "The Bear",
+    "photo": null,
+    "favoriteTeam": null,
+  },
+  {
+    "managerID": "1132151539948347392",
+    "name": "Rhodes x Pags",
+    "coManagerID": "1263967779644518400",
+    "location": "Dallas, TX",
+    "school": "Villanova x Northwestern",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": "ne",
+  },
+  {
+    "managerID": "1132160162426511360",
+    "name": "J-Bone",
+    "location": "Dallas, TX",
+    "school": "Texas Tech",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": "dal",
+  },
+  {
+    "managerID": "1132160905275404288",
+    "name": "Vigo",
+    "location": "Dallas, TX",
+    "school": "Alabama",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": null,
+  },
+  {
+    "managerID": "1132162695655997440",
+    "name": "Ryan",
+    "location": "Somewhere in California",
+    "school": "United States Coast Guard Academy",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": null,
+  },
+  {
+    "managerID": "1132179431537242112",
+    "name": "Charles",
+    "location": "Dallas, TX",
+    "school": "TCU",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": "chi",
+  },
+  {
+    "managerID": "1132448998603829248",
+    "name": "Josh",
+    "location": "Denver, CO",
+    "school": "Indiana",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": "chi",
+  },
+  {
+    "managerID": "773337849662889984",
+    "name": "JQ",
+    "location": "Cincinatti, OH",
+    "school": "Alabama",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": "pit",
+  },
+  {
+    "managerID": "1256721112734121984",
+    "name": "T2",
+    "location": "Dallas, TX",
+    "school": "Alabama",
+    "bio": null,
+    "photo": null,
+    "favoriteTeam": null,
+  },
+]
   
   
   /*   !!  !!  IMPORTANT  !!  !! */
