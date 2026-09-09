27324ac2cfa4d88ccb0d5baf3dc7889e37873e61
c0derad <conradjbailey@outlook.com>
2026-09-09 14:49:28 -0500

    Add page.js for league matchups and team managers

---
+++ src/routes/preview/+page.js
@@ -0,0 +1,16 @@
+import {
+    getLeagueMatchups,
+    getLeagueTeamManagers,
+    loadPlayers
+} from '$lib/utils/helper';
+
+export async function load({ url, fetch }) {
+    const queryWeek = url?.searchParams?.get('week');
+
+    return {
+        queryWeek: isNaN(queryWeek) ? null : queryWeek,
+        matchupsData: getLeagueMatchups(),
+        leagueTeamManagersData: getLeagueTeamManagers(),
+        playersData: loadPlayers(fetch),
+    };
+}
