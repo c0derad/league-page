201321ad1541138f502fc7f44fc3de365b5c5cb5
c0derad <conradjbailey@outlook.com>
2026-09-15 09:21:59 -0500

    Update and rename +page.js to +page.server.js

---
+++ src/routes/recap/+page.server.js
@@ -0,0 +1,44 @@
+import {
+    getLeagueTeamManagers,
+    loadPlayers
+} from '$lib/utils/helper';
+
+import { leagueID } from '$lib/utils/leagueInfo';
+
+import {
+    getWeeklyNFLStats
+} from '$lib/utils/nflStats.server';
+
+export async function load({ url, fetch }) {
+    const queryWeek =
+        Number(url.searchParams.get('week')) || 1;
+
+    const matchupResponse = await fetch(
+        `https://api.sleeper.app/v1/league/${leagueID}/matchups/${queryWeek}`
+    );
+
+    if (!matchupResponse.ok) {
+        throw new Error(
+            `Unable to load Sleeper matchups: ${matchupResponse.status}`
+        );
+    }
+
+    const rawMatchups =
+        await matchupResponse.json();
+
+    const nflStats =
+        await getWeeklyNFLStats(
+            fetch,
+            queryWeek
+        );
+
+    return {
+        week: queryWeek,
+        rawMatchups,
+        nflStats,
+        leagueTeamManagersData:
+            getLeagueTeamManagers(),
+        playersData:
+            loadPlayers(fetch)
+    };
+}
