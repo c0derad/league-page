191b87bbd2c7acbb5ab3c1f3969fac7aef3d3afd
c0derad <conradjbailey@outlook.com>
2026-09-15 10:30:38 -0500

    Refactor load function to handle next matchups

--- src/routes/recap/+page.server.js
+++ src/routes/recap/+page.server.js
@@ -3,28 +3,57 @@ import {
     loadPlayers
 } from '$lib/utils/helper';
 
-import { leagueID } from '$lib/utils/leagueInfo';
+import {
+    leagueID
+} from '$lib/utils/leagueInfo';
 
 import {
     getWeeklyNFLStats
 } from '$lib/utils/nflStats.server';
 
-export async function load({ url, fetch }) {
+export async function load({
+    url,
+    fetch
+}) {
     const queryWeek =
-        Number(url.searchParams.get('week')) || 1;
+        Number(
+            url.searchParams.get(
+                'week'
+            )
+        ) || 1;
 
-    const matchupResponse = await fetch(
-        `https://api.sleeper.app/v1/league/${leagueID}/matchups/${queryWeek}`
-    );
+    const [
+        matchupResponse,
+        nextMatchupResponse
+    ] = await Promise.all([
+        fetch(
+            `https://api.sleeper.app/v1/league/${leagueID}/matchups/${queryWeek}`
+        ),
+
+        fetch(
+            `https://api.sleeper.app/v1/league/${leagueID}/matchups/${queryWeek + 1}`
+        )
+    ]);
 
     if (!matchupResponse.ok) {
         throw new Error(
-            `Unable to load Sleeper matchups: ${matchupResponse.status}`
+            `Unable to load Sleeper Week ${queryWeek} matchups: ${matchupResponse.status}`
         );
     }
 
-    const rawMatchups =
-        await matchupResponse.json();
+    if (!nextMatchupResponse.ok) {
+        throw new Error(
+            `Unable to load Sleeper Week ${queryWeek + 1} matchups: ${nextMatchupResponse.status}`
+        );
+    }
+
+    const [
+        rawMatchups,
+        nextRawMatchups
+    ] = await Promise.all([
+        matchupResponse.json(),
+        nextMatchupResponse.json()
+    ]);
 
     const nflStats =
         await getWeeklyNFLStats(
@@ -33,11 +62,18 @@ export async function load({ url, fetch }) {
         );
 
     return {
-        week: queryWeek,
+        week:
+            queryWeek,
+
         rawMatchups,
+
+        nextRawMatchups,
+
         nflStats,
+
         leagueTeamManagersData:
             getLeagueTeamManagers(),
+
         playersData:
             loadPlayers(fetch)
     };
