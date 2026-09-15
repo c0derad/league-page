ed5169898e46e46572219e61f1b1f2e8a367c47e
c0derad <conradjbailey@outlook.com>
2026-09-15 15:19:43 -0500

    Refactor roster page to enhance loading and snapshot display

--- src/routes/rosters/+page.svelte
+++ src/routes/rosters/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import LinearProgress from '@smui/linear-progress';
-	import { Rosters } from '$lib/components'
+	import { Rosters } from '$lib/components';
 
 	export let data;
+
 	const rostersInfo = data.rostersInfo;
 </script>
 
@@ -11,11 +12,42 @@
 		position: relative;
 		z-index: 1;
 	}
+
 	.loading {
 		display: block;
 		width: 85%;
 		max-width: 500px;
 		margin: 80px auto;
+	}
+
+	.snapshotHolder {
+		width: 95%;
+		max-width: 1200px;
+		margin: 3em auto;
+	}
+
+	.snapshotDetails {
+		border: 1px solid var(--ccc);
+		border-radius: 0.5em;
+		padding: 1em;
+	}
+
+	.snapshotSummary {
+		cursor: pointer;
+		font-weight: 700;
+	}
+
+	.snapshotCode {
+		white-space: pre-wrap;
+		word-break: break-word;
+		margin-top: 1em;
+		padding: 1em;
+		border: 1px solid var(--ccc);
+		border-radius: 0.5em;
+		font-size: 0.75em;
+		overflow-x: auto;
+		max-height: 800px;
+		overflow-y: auto;
 	}
 </style>
 
@@ -26,11 +58,43 @@
 			<br />
 			<LinearProgress indeterminate />
 		</div>
+
 	{:then [leagueData, rosterData, leagueTeamManagers, playersInfo]}
-		<!-- promise was fulfilled -->
-		<Rosters {leagueData} {rosterData} {leagueTeamManagers} {playersInfo} /> <!-- displays rosters -->
+
+		<Rosters
+			{leagueData}
+			{rosterData}
+			{leagueTeamManagers}
+			{playersInfo}
+		/>
+
+		<div class="snapshotHolder">
+			<details class="snapshotDetails">
+
+				<summary class="snapshotSummary">
+					DEBUG: WEEK 1 ROSTER SNAPSHOT
+				</summary>
+
+				<pre class="snapshotCode">{JSON.stringify(
+					{
+						leagueData,
+						rosterData,
+						leagueTeamManagers,
+						playersInfo
+					},
+					null,
+					2
+				)}</pre>
+
+			</details>
+		</div>
+
 	{:catch error}
-		<!-- promise was rejected -->
-		<p>Something went wrong: {error.message}</p>
+
+		<p>
+			Something went wrong:
+			{error.message}
+		</p>
+
 	{/await}
 </div>
