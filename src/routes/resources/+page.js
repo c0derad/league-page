ec1330ed312e04072c0260bd523161ad8559c34d
c0derad <conradjbailey@outlook.com>
2026-09-16 12:10:51 -0500

    Add data parameter to load function

--- src/routes/resources/+page.js
+++ src/routes/resources/+page.js
@@ -1,9 +1,10 @@
 import { getNews } from '$lib/utils/helper';
 
-export async function load({fetch}) {
+export async function load({ fetch, data }) {
     const articlesData = getNews(fetch);
 
     return {
+        ...data,
         articlesData
     };
 }
