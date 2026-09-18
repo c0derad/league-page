5c0b5f1034e41939866b7272e3e3ecdee992e076
c0derad <conradjbailey@outlook.com>
2026-09-18 10:18:40 -0500

    Add page.js for loading best bets data

---
+++ src/routes/best-bets/+page.js
@@ -0,0 +1,25 @@
+import { weeklyBestBets } from '$lib/utils/weeklyBestBets';
+
+export function load({ url }) {
+    const availableWeeks = Object.keys(weeklyBestBets)
+        .map(Number)
+        .sort((a, b) => a - b);
+
+    const latestPublishedWeek =
+        availableWeeks[availableWeeks.length - 1] || 1;
+
+    const requestedWeek =
+        Number(url.searchParams.get('week'));
+
+    const week =
+        availableWeeks.includes(requestedWeek)
+            ? requestedWeek
+            : latestPublishedWeek;
+
+    return {
+        week,
+        availableWeeks,
+        bestBetsData:
+            weeklyBestBets[week]
+    };
+}
