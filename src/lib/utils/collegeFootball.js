d9466025781d24842946215bbfbbf941f4d7af5d
c0derad <conradjbailey@outlook.com>
2026-09-18 10:37:29 -0500

    Add function to fetch college football week data

---
+++ src/lib/utils/collegeFootball.js
@@ -0,0 +1,20 @@
+const NCAA_API =
+    'https://ncaa-api.henrygd.me';
+
+export async function getCollegeFootballWeek(
+    fetch,
+    year,
+    week
+) {
+    const response = await fetch(
+        `${NCAA_API}/scoreboard/football/fbs/${year}/${week}/all-conf`
+    );
+
+    if (!response.ok) {
+        throw new Error(
+            `Failed to load NCAA Week ${week}`
+        );
+    }
+
+    return await response.json();
+}
